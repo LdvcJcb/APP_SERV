5c38154ad7ef3ceed0f3776a9ede5cbb0fc4ec05
kryyshna <33688650+kryyshna@users.noreply.github.com>
2017-12-13 10:50:59 +0100

    Add files via upload
    abonne fini

--- src/client/Abonne.java
+++ src/client/Abonne.java
@@ -2,4 +2,25 @@ package client;
 
 public class Abonne {
 
+	private int num;
+	private String nom;
+	
+	public Abonne(int num, String nom) {
+		this.num = num;
+		this.nom = nom;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Abonne [num=" + num + ", nom=" + nom + "]";
+	}
+
 }
