7775f143c80fa2e75929de7f26bbbf3ea9509006
Ludovic JACOB <jacob@iut.univ-paris5.fr>
2017-12-13 11:35:37 +0100

    suppre

--- src/serveur/ServeurEmprunt.java
+++
@@ -1,7 +0,0 @@
-package serveur;
-
-public class ServeurEmprunt {
-	
-	@SuppressWarnings("unused")
-	private final int PORT_RESERVATION = 2600;
-}
--- src/serveur/services/ServiceReservation.java
+++
@@ -1,5 +0,0 @@
-package serveur.services;
-
-public class ServiceReservation {
-
-}
